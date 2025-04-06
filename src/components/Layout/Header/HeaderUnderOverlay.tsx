83db12a070fb561d835349458029f7752f6d7f96
nhatnguyen1873 <nhatnguyen1873@gmail.com>
2025-04-06 06:35:43 +0000

    chore: utilize ResizeObserver in HeaderUnderOverlay

--- src/components/Layout/Header/HeaderUnderOverlay.tsx
+++ src/components/Layout/Header/HeaderUnderOverlay.tsx
@@ -1,31 +1,59 @@
 import { cn } from '@/lib/utils';
-import { memo, type ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  type CSSProperties,
+  memo,
+  type ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 export interface HeaderUnderOverlayProps {
   className?: string;
   rootClassName?: string;
+  overlayClassName?: string;
   children?: ReactNode;
 }
 
 export const HeaderUnderOverlay = memo<HeaderUnderOverlayProps>(
-  ({ className, rootClassName, children }) => {
+  ({ className, rootClassName, overlayClassName, children }) => {
     const navbarRef = useRef<HTMLDivElement>(null);
     const [navbarHeight, setNavbarHeight] = useState(0);
 
     useEffect(() => {
-      if (navbarRef.current) {
-        setNavbarHeight(navbarRef.current.offsetHeight);
-      }
+      if (!navbarRef.current) return;
+
+      const navbarElement = navbarRef.current;
+
+      const updateNavbarHeight = () => {
+        setNavbarHeight(navbarElement.offsetHeight);
+      };
+
+      updateNavbarHeight();
+
+      const resizeNavHeightObs = new ResizeObserver(updateNavbarHeight);
+      resizeNavHeightObs.observe(navbarElement);
+
+      window.addEventListener('resize', updateNavbarHeight);
+
+      return () => {
+        window.removeEventListener('resize', updateNavbarHeight);
+        resizeNavHeightObs.disconnect();
+      };
     }, []);
 
     return (
       <header className={cn('relative', rootClassName)}>
         <div
-          className='pointer-events-none absolute inset-x-0 bg-gradient-to-b from-white/85 backdrop-blur [mask:linear-gradient(white,white,transparent)]'
-          style={{
-            height: `${navbarHeight * 1.5}px`,
-            clipPath: `inset(${navbarHeight}px 0 0 0)`,
-          }}
+          className={cn(
+            'pointer-events-none absolute inset-x-0 h-[calc(var(--navbar-height)*3/2)] bg-gradient-to-b from-white/85 backdrop-blur [clip-path:inset(var(--navbar-height)_0_0_0)] [mask:linear-gradient(white,white,transparent)]',
+            overlayClassName,
+          )}
+          style={
+            {
+              '--navbar-height': `${navbarHeight}px`,
+            } as CSSProperties
+          }
         />
         {/* <div className='pointer-events-none absolute inset-x-0 h-[calc(var(--navbar-height)*3/2)] bg-gradient-to-b from-white/85 backdrop-blur duration-200 [mask:linear-gradient(white,white,transparent)]'></div> */}
         <div ref={navbarRef} className={className}>
