d9985e7c66d737bc6ddc8cb6f28f07a0c197ce51
nhatnguyen1873 <nhatnguyen1873@gmail.com>
2025-04-06 10:45:47 +0000

    feat: [HeaderUnderOverlay] only display linear gradient when scrolling down

--- src/components/Layout/Header/HeaderUnderOverlay.tsx
+++ src/components/Layout/Header/HeaderUnderOverlay.tsx
@@ -18,7 +18,9 @@ export const HeaderUnderOverlay = memo<HeaderUnderOverlayProps>(
   ({ className, overlayClassName, children }) => {
     const headerRef = useRef<HTMLDivElement>(null);
     const [headerHeight, setHeaderHeight] = useState(0);
+    const [isScrolled, setIsScrolled] = useState(false);
 
+    console.log('HeaderUnderOverlay');
     useEffect(() => {
       if (!headerRef.current) return;
 
@@ -28,15 +30,22 @@ export const HeaderUnderOverlay = memo<HeaderUnderOverlayProps>(
         setHeaderHeight(headerElement.offsetHeight);
       };
 
+      const handleScroll = () => {
+        setIsScrolled(window.scrollY > 0);
+      };
+
       updateHeaderHeight();
+      handleScroll();
 
       const resizeHeaderHeightObs = new ResizeObserver(updateHeaderHeight);
       resizeHeaderHeightObs.observe(headerElement);
 
       window.addEventListener('resize', updateHeaderHeight);
+      window.addEventListener('scroll', handleScroll);
 
       return () => {
         window.removeEventListener('resize', updateHeaderHeight);
+        window.removeEventListener('scroll', handleScroll);
         resizeHeaderHeightObs.disconnect();
       };
     }, []);
@@ -44,17 +53,25 @@ export const HeaderUnderOverlay = memo<HeaderUnderOverlayProps>(
     return (
       <header ref={headerRef} className={cn('relative', className)}>
         <div
-          className={cn(
-            'pointer-events-none absolute inset-x-0 top-0 h-[calc(var(--navbar-height)*3/2)] bg-gradient-to-b from-white/85 backdrop-blur [clip-path:inset(var(--navbar-height)_0_0_0)] [mask:linear-gradient(white,white,transparent)]',
-            overlayClassName,
-          )}
           style={
             {
               '--navbar-height': `${headerHeight}px`,
             } as CSSProperties
           }
+          className={cn(
+            'pointer-events-none absolute inset-x-0 top-0 h-[calc(var(--navbar-height)*1.75)] bg-gradient-to-b from-white/85 opacity-0 backdrop-blur transition-[height,opacity] duration-300 [clip-path:inset(var(--navbar-height)_0_0_0)] [mask:linear-gradient(white,white,transparent)]',
+            isScrolled && 'opacity-100',
+            overlayClassName,
+          )}
         />
-        {/* <div className='pointer-events-none absolute inset-x-0 h-[calc(var(--navbar-height)*3/2)] bg-gradient-to-b from-white/85 backdrop-blur duration-200 [mask:linear-gradient(white,white,transparent)]'></div> */}
+        {/* <div
+          style={
+            {
+              '--navbar-height': `${headerHeight}px`,
+            } as CSSProperties
+          }
+          className='pointer-events-none absolute inset-x-0 top-0 h-[calc(var(--navbar-height)*1.75)] bg-gradient-to-b from-white/85 backdrop-blur duration-200 [clip-path:inset(var(--navbar-height)_0_0_0)] [mask:linear-gradient(white,white,transparent)]'
+        ></div> */}
         {children}
       </header>
     );
