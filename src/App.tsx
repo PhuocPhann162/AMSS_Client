70c1d37fd7c117dcf271dd78986b19e149d99c5a
nhatnguyen1873 <nhatnguyen1873@gmail.com>
2025-05-29 18:18:27 +0000

    feat: init lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { useGetCountriesQuery } from '@/api';
+import { cancelFrame, frame } from 'framer-motion';
+import { ReactLenis, type LenisRef } from 'lenis/react';
+import { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import router from './route';
 import { setCountries } from './storage/redux/countrySlice';
-import { useGetCountriesQuery } from '@/api';
 
 function App() {
   const dispatch = useDispatch();
   const { data } = useGetCountriesQuery();
+
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
+  }, []);
 
   useEffect(() => {
     if (data) {
@@ -17,12 +32,15 @@ function App() {
   }, [data]);
 
   return (
-    <RouterProvider
-      router={router}
-      future={{
-        v7_startTransition: true,
-      }}
-    />
+    <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }}
+      />
+    </>
   );
 }
 
