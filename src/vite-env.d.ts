dfbf3095223139353e737cde8e96207187ce8f44
nhatnguyen1873 <nhatnguyen1873@gmail.com>
2025-06-21 12:08:22 +0000

    feat: env typescript

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,11 @@ interface ImportMetaEnv {
   readonly VITE_AGRO_API_KEY?: string;
   readonly VITE_PLANT_API_KEY?: string;
   readonly VITE_MAPBOX_ACESS_TOKEN?: string;
+  readonly VITE_STRIPE_PUBLISH_KEY?: string;
+  readonly VITE_STRIPE_SECRET_KEY?: string;
+  readonly VITE_PLANT_NET_API_KEY?: string;
+  readonly VITE_GOOEY_API_KEY?: string;
+  readonly VITE_SERVER_BASE_URL?: string;
 }
 
 interface ImportMeta {
